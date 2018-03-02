c11d36ffd8a1993aec5b50211576c630af780e4f
SChernuyk <sergey@taganteam.com>
2018-03-02 11:45:39 +0300

    .

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-require 'devise'
 Bundler.require(*Rails.groups)
 
 
